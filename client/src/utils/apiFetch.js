cd16a5a90c8e8f324fd2b83e4f2a64a947613258
PCDanil_Myagkiy <danilsoft1011@gmail.com>
2026-08-31 19:39:00 +0300

    Fetch testing update

--- client/src/utils/apiFetch.js
+++ client/src/utils/apiFetch.js
@@ -3,6 +3,7 @@ import { fetchRefresh } from "./fetchRefresh.js";
 
 
 const API_URL = import.meta.env.VITE_API_URL;
+console.log(`API_URL: ${API_URL}`);
 
 
 const apiFetch = (endpoint, type, options = {}) => {
