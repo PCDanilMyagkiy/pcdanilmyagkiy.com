de3d57b9f74a753e1bd1da5f5a060c8b0685c0b3
PCDanil_Myagkiy <danilsoft1011@gmail.com>
2026-08-31 18:00:09 +0300

    apiFetch added

---
+++ client/src/utils/apiFetch.js
@@ -0,0 +1,17 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+
+const apiFetch = (endpoint, type, options = {}) => {
+    switch (type) {
+        case "normal":
+            return fetch(`${API_URL}${endpoint}`, {
+                ...options,
+                credentials: "include"
+            });
+        case "refresh":
+            return fetchRefresh(`${API_URL}${endpoint}`, options);
+    }
+}
+
+
+export default apiFetch;
